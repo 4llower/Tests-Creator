69f0a1a69d234dff57e37a2a85ae3ec761242256
4llower <i.4llower@gmail.com>
2020-05-06 18:05:23 +0300

    Added support of many tests, added functionality to pass test and calculate result.

--- Test.h
+++ Test.h
@@ -3,6 +3,9 @@
 #include "TestModel.h"
 #include <fstream>
 #include <iostream>
+#include <exception>
+#include <algorithm>
+#include <iomanip>
 
 class Test {
 
@@ -10,6 +13,7 @@ private:
 	std::vector<TestModel> testData;
 	void getTestData(std::string);
 	TestModel decodeQuestionString(std::string);
+	int getNumber(int, int);
 public:
 	void chooseTest();
 	void startTest(std::string);
---
+++ TestController.h
@@ -0,0 +1,12 @@
+#include <vector>
+#include <string>
+#include <fstream>
+
+static class TestController
+{
+public:
+	static std::vector<std::string> getTestList();
+	static void addNewTest(std::string);
+	static void eraseTest(std::string);
+};
+
